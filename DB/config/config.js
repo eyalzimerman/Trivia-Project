048d3a6f8334f2fcea1fc4d200c7e250b9f5fc2c
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-04-21 16:16:36 +0300

    config

--- DB/config/config.js
+++ DB/config/config.js
@@ -1,15 +1,16 @@
+require("dotenv").config();
 module.exports = {
   development: {
     username: "root",
-    password: null,
-    database: "database_development",
+    password: process.env.SQL_PASSWORD,
+    database: process.env.DB_NAME,
     host: "127.0.0.1",
     dialect: "mysql",
   },
   test: {
     username: "root",
-    password: null,
-    database: "database_test",
+    password: process.env.SQL_PASSWORD,
+    database: process.env.DB_NAME,
     host: "127.0.0.1",
     dialect: "mysql",
   },
