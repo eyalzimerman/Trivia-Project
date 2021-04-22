89faa18522e88100542a5e1c1f92d430771cf35e
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-04-22 21:49:30 +0300

    basic start client

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const api = require("./api");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", api);
 
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require("./app");
-const PORT = 3000;
+
+const PORT = 8080;
 
 app.listen(PORT, () => {
   console.log(`app listening on port: ${PORT}`);
