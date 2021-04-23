282ccd883f9ce7748deecb4a72b3a32aba538f15
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-04-23 12:14:04 +0300

    fixed the right answer on button click

--- client/src/components/Answer.js
+++ client/src/components/Answer.js
@@ -1,5 +1,22 @@
 import React from "react";
 import decypter from "../Utils/decypter";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
+const StyledButton = withStyles({
+  root: {
+    background:
+      "linear-gradient(0deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 31%, rgba(0,212,255,1) 100%)",
+    borderRadius: 3,
+    border: 0,
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+  },
+  label: {
+    textTransform: "capitalize",
+  },
+})(Button);
 
 export default function Answer({
   answer,
@@ -8,8 +25,13 @@ export default function Answer({
   question,
 }) {
   return (
-    <div onClick={(e) => answerClickEvent(e.target.innerText)}>
-      {answer.option}{" "}
+    <div>
+      <StyledButton
+        onClick={(e) => answerClickEvent(e.target.innerText)}
+        variant="outlined"
+      >
+        {answer.option}
+      </StyledButton>
       <span>
         {isAnswerVisible && question.user.allAnswers.length === 4
           ? decypter(answer.answer)
