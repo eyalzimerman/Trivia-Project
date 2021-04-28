f596b3cbab063e4175346e3e987c6974d13bb0e6
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-04-28 13:15:54 +0300

    added cookieValidator middleware

--- api/index.js
+++ api/index.js
@@ -5,7 +5,22 @@ const jwt = require("jsonwebtoken");
 
 const api = Router();
 
-api.use("/trivia", trivia);
+const cookieValidator = (req, res, next) => {
+  let token = req.get("Access-Token");
+  if (!token) {
+    return res.status(401).json({ message: "Access token required" });
+  }
+  token = token.slice(7);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid access token" });
+    } else {
+      next();
+    }
+  });
+};
+
+api.use("/trivia", cookieValidator, trivia);
 api.use("/users", users);
 
 module.exports = api;
