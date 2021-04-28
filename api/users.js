f9f483d8184c99de5d4754f7c5b05ce6cb4b6cbd
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-04-28 13:10:15 +0300

    added route to post score to DB

--- api/users.js
+++ api/users.js
@@ -38,12 +38,13 @@ router.post("/register", async (req, res) => {
   }
 
   const accessToken = createAccessToken(req.body);
-  const refreshToken = createAccessToken(req.body);
+  const refreshToken = createRefreshToken(req.body);
   res.cookie("Access-Token", `Bearer ${accessToken}`);
   res.cookie("Refresh-Token", `Bearer ${refreshToken}`);
   return res.status(200).json({ message: "User created successfully" });
 });
 
+// User login
 router.post("/login", async (req, res) => {
   const { name, password } = req.body;
   const doesExist = await checkUserExist(req.body);
@@ -56,9 +57,22 @@ router.post("/login", async (req, res) => {
     return res.status(400).json({ error: "Username or password is incorrect" });
   } else {
     const accessToken = createAccessToken(req.body);
-    const refreshToken = createAccessToken(req.body);
+    const refreshToken = createRefreshToken(req.body);
     res.cookie("Access-Token", `Bearer ${accessToken}`);
     res.cookie("Refresh-Token", `Bearer ${refreshToken}`);
     return res.status(200).json({ message: "login successfully" });
   }
 });
+
+// Adds user with score to scoreboard
+router.post("/score", async (req, res) => {
+  const { id, name, score } = req.body;
+  try {
+    await addScore({ id, name, score });
+    return res.status(200).json({ message: "Score added" });
+  } catch (error) {
+    return res.status(500).json({ error: "Could not add score" });
+  }
+});
+
+module.exports = router;
