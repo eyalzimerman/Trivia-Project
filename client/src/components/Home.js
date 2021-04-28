f0256a721f38e1364fd3e002e492e47db10b023f
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-04-28 13:31:29 +0300

    fixed home component, without inputs

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,37 +2,24 @@ import React, { useRef } from "react";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 
-export default function Home({ inputHandler, userName }) {
-  const inputRef = useRef();
+export default function Home({ setUserExists }) {
+  const logoutHandler = async () => {
+    try {
+      await axios.post("/api/users/logout");
+      setUserExists(false);
+    } catch (error) {
+      console.log("error logout");
+    }
+  };
   return (
     <div id="home-container">
       <h1 id="main-title">Trivia Game</h1>
       <div id="login-container">
-        <input
-          id="username-input"
-          ref={inputRef}
-          type="text"
-          placeholder="Enter user name"
-          onChange={(e) => inputHandler(e.target.value)}
-          required
-        />
-        {userName === "" ? (
-          <Link to="/">
-            <Button
-              className="start-game-button"
-              onClick={() => inputRef.current.focus()}
-              variant="contained"
-            >
-              Start Game
-            </Button>
-          </Link>
-        ) : (
-          <Link to="/game">
-            <Button className="start-game-button" variant="contained">
-              Start Game
-            </Button>
-          </Link>
-        )}
+        <Link to="/game">
+          <Button className="start-game-button" variant="contained">
+            Start Game
+          </Button>
+        </Link>
         <Link to="/scoreboard">
           <Button
             className="scoreboard-button"
@@ -42,6 +29,15 @@ export default function Home({ inputHandler, userName }) {
             Scoreboard
           </Button>
         </Link>
+
+        <Button
+          onClick={logoutHandler}
+          className="scoreboard-button"
+          variant="contained"
+          color="primary"
+        >
+          Logout
+        </Button>
       </div>
     </div>
   );
